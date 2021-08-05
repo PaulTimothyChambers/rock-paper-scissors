11375f5b2f7275cd9e20496f3d8b815043b2397b
Paul Chambers <paultimothychambers@gmail.com>
2021-08-05 11:39:58 -0600

    Build score modifier for determining classic winner

--- Game.js
+++ Game.js
@@ -1,15 +1,56 @@
+var human = require('./Player.js');
+
 class Game {
   constructor() {
-    this.humanChoice = '';
-    this.computerChoice = '';
-  }
-  
-  logWinner() {
-
+    this.human = ;
+    this.computer = ;
   }
 
-  determineChoice() {
+  logWinner(human, computer) {
 
+  }
+
+  determineHumanChoice(playerOne, playerTwo, event) {
+    var humanChoice = event.target;
+
+    if (humanChoice.className === 'block__block--paper-choice') {
+      playerTwo.scissorsChoice += 20
+      playerTwo.rockChoice -= 20;
+      var humanScore = playerOne.paperScore
+      return humanScore
+
+    } else if (humanChoice.className === 'block__block--rock-choice') {
+      playerTwo.paperChoice += 20
+      playerTwo.scissorsChoice -= 20;
+      var humanScore = playerOne.rockScore
+      return humanScore
+
+    } else if (humanChoice.className === 'block__block--scissors-choice') {
+      playerTwo.rockChoice += 20
+      playerTwo.paperChoice -= 20;
+      var humanScore = playerOne.scissorsScore
+      return humanScore
+    }
+  }
+
+  playerChoice(choice) {
+
+  }
+
+  determineComputerChoice() {
+    RNG
+  }
+
+  determineWinner() {
+    event.preventDefault()
+
+    this.human = new Player();
+    this.computer = new Player();
+
+
+    // var selectedParent = docQuery, selectedParent.addListener(invoke determineWinner) <<<< goes in main.js
+
+    var humanTotalScore = determineHumanChoice(this.human, this.computer);
   }
 
   saveWinner() {
