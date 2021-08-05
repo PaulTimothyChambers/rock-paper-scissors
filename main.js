3d307b523cfedd50b843d8d9ae7c2af51b46c4d1
Paul Chambers <paultimothychambers@gmail.com>
2021-08-05 17:36:49 -0600

    Begin building control flow

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
+var startClassicGame = document.querySelector('.game-board__classic-game-btn')
+var newGame = new Game()
+var player = new Player()
+var gameMethod = newGame.determineWinner()
+
+startClassicGame.addEventListener('click', gameInPlay)
+
+function gameInPlay() {
+  event.preventDefault()
+  newGame.determineWinner();
+
+  newGame.determineClassicScores(newGame.determineComputerChoice(['paper', 'rock', 'scissors']), newGame.human);
+
+  newGame.playerChoice(newGame.human);
+  console.log(newGame.determineComputerChoice(['paper', 'rock', 'scissors']));
+
+  newGame.logWinner(newGame.humanScore, newGame.human.score)
+}
+
 function changeViewToOutcome() {
 
 }
