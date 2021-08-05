142f40f3e30dca6999fc96e454fab893fd25578a
Paul Chambers <paultimothychambers@gmail.com>
2021-08-05 15:40:23 -0600

    Add needed winCount property

--- Player.js
+++ Player.js
@@ -3,15 +3,16 @@ class Player {
     this.paperScore = 20;
     this.rockScore = 20;
     this.scissorsScore = 20;
-    this.wins = 0;
+    this.score = 20;
+    this.winCount = 0;
   }
 
-  saveWinToStorage() {
-
+  saveWinToStorage(player) {
+    // localstorage set item (player, stringify-this)
   }
 
   retrieveWins() {
-
+    // local storage .length   WON'T WORK PROBABLY: NEED THIS TO FUNCTION AS INSTANCE OF COMP, TOO
   }
 
   takeTurn() {
