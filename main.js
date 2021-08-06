d068d137187700de09a66c0669fff7b5bace2d64
Paul Chambers <paultimothychambers@gmail.com>
2021-08-06 09:45:41 -0600

    Add query selectors and refactor

--- main.js
+++ main.js
@@ -1,11 +1,33 @@
-var startClassicGame = document.querySelector('.game-board__classic-game-btn')
-var newGame = new Game()
-var gameMethod = newGame.determineWinner()
+var startClassicGame = document.querySelector('.game-board__classic-game-btn');
+var btnClassicGame = document.querySelector('.game-board__classic-game-button');
+var btnDifficultGame = document.querySelector('.game-board__difficult-game-button');
+var rockChoice = document.querySelector('.classic-card__rock');
+var paperChoice = document.querySelector('.classic-card__paper');
+var scissorsChoice = document.querySelector('.classic-card__scissors');
+var rockChoiceDifficult = document.querySelector('.difficult-card__rock');
+var paperChoiceDifficult = document.querySelector('.difficult-card__paper');
+var scissorsChoiceDifficult = document.querySelector('.difficult-card__scissors');
+var alienChoiceDifficult = document.querySelector('.difficult-card__alien');
+var biggerAlienChoiceDifficult = document.querySelector('.difficult-card__bigger-alien');
+var wimpyAlienChoiceDifficult = document.querySelector('.difficult-card__wimpy-alien');
+var changeGameMode = document.querySelector('.bottom-border__change-game-button')
 
-startClassicGame.addEventListener('click', gameInPlay) // <<<change this, don't forget
+changeGameMode.addEventListener('click', changeViewToMain);
+rockChoice.addEventListener('click', gameInPlay); // <<<change this, don't forget
+paperChoice.addEventListener('click', gameInPlay);
+scissorsChoice.addEventListener('click', gameInPlay);
+rockChoiceDifficult.addEventListener('click', gameInPlay);
+paperChoiceDifficult.addEventListener('click', gameInPlay);
+scissorsChoiceDifficult.addEventListener('click', gameInPlay);
+alienChoiceDifficult.addEventListener('click', gameInPlay);
+biggerAlienChoiceDifficult.addEventListener('click', gameInPlay);
+wimpyAlienChoiceDifficult.addEventListener('click', gameInPlay);
+
+
 
 function gameInPlay() {
-  event.preventDefault()
+  var newGame = new Game();
+  event.preventDefault();
   newGame.determineWinner();
 
   newGame.determineClassicScores(newGame.determineComputerChoice(['paper', 'rock', 'scissors']), newGame.human);
@@ -15,7 +37,7 @@ function gameInPlay() {
 
   newGame.playerChoice(humanChoice);
 
-  newGame.logWinner(newGame.humanScore, newGame.human.score)
+  newGame.logWinner(newGame.humanScore, newGame.human.score);
 
   // if (newGame.logWinner(newGame.humanScore, newGame.human.score) === 1) {
   //   newGame.computer.winCount += 1
@@ -33,6 +55,10 @@ function gameInPlay() {
   //   setTimeout(showGameChangeButton, 3000)
   //   return
   // }
+}
+
+function changeViewToMain() {
+
 }
 
 function changeViewToClassic() {
